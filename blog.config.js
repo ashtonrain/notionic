81483427dba554a8049ba0fbe6d86485c864f4e1
ashtonrain <nsmbro@gmail.com>
2023-01-09 00:07:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
   previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
-  defaultCover: '/cover.jpg',
+  defaultCover: '',
   socialLink: {
     twitter: 'https://twitter.com/ashtonra1n',
     github: 'https://github.com/ashtonrain',
